2bc3d60ee3ae5e0c6ea1a528ccfb7ce25c688bee
Patrik Bergman <patrik.bergman@transmode.com>
2015-10-26 14:10:54 +0100

    started working on enhanced builder

---
+++ src/main/java/se/patrikbergman/java/designpattern/builder/enhanced/Band.java
@@ -0,0 +1,52 @@
+package se.patrikbergman.java.designpattern.builder.enhanced;
+
+class Band {
+    private final String name;
+    private final String description;
+    private final int yearsActive;
+
+    private Band(Builder builder) {
+        name = builder.name;
+        description = builder.description;
+        yearsActive = builder.yearsActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getYearsActive() {
+        return yearsActive;
+    }
+
+    public static final class Builder {
+        private String name;
+        private String description;
+        private int yearsActive;
+
+        public Builder() { }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder yearsActive(int yearsActive) {
+            this.yearsActive = yearsActive;
+            return this;
+        }
+
+        public Band build() {
+            return new Band(this);
+        }
+    }
+}
